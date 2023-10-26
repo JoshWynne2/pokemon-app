688a9242cce6c327178fed53503a8bb1543f11ec
JoshWynne2 <N00221586@student.iadt.ie>
2023-10-27 00:06:08 +0100

    pokemon index

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pokemon;
+use DB;
 use Illuminate\Http\Request;
 
 class PokemonController extends Controller
@@ -13,7 +14,13 @@ class PokemonController extends Controller
     public function index()
     {
 
-		$allpokemon = Pokemon::all()->get();
+		// $allpokemon = DB::table('pokemon')->where('type_id', '2')->get();
+
+		$allpokemon = DB::table('pokemon as p')
+						->select('p.name', "t.name as type", "t2.name as secondary_type")
+						->join('types as t', 'p.type_id', '=', 't.id')
+						->join('types as t2', 'p.type_secondary_id', '=', 't2.id')
+						->get();
 
         return view('pokemon.index', ['pokemon' => $allpokemon]);
     }
---
+++ resources/views/pokemon/index.blade.php
@@ -0,0 +1,43 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('All Pokemon') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+					<table class="w-full">
+						<thead class="bg-slate-600"> 
+							<tr>
+							<th class="px-4 py-2">Name</th>
+							<th class="px-4 py-2">Type</th>
+							<th class="px-4 py-2">Secondary Type</th>
+							<!-- <th class="px-4 py-2">4</th> -->
+							</tr>
+						</thead>
+						<tbody> 
+					@forelse ($pokemon as $mon)
+					@if ($loop->iteration % 2 == 0)
+						<tr class="bg-slate-900 hover:bg-slate-700">
+					@else
+						<tr class ="bg-slate-800 hover:bg-slate-700">
+					@endif 
+							<td class="px-3 w-2/4"> {{$mon->name}}</td>
+							<td class="px-3 w-1/4"> {{$mon->type}}</td>
+							<td class="px-3 w-1/4"> {{$mon->secondary_type}}</td>
+							<!-- <td class="px-3 w-2/6"> {{$mon->name}}</td> -->
+						</tr>
+
+					@empty
+						<h4> You have no pokemon! </h4>
+					@endforelse
+						</tbody>
+					</table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
